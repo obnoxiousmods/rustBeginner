9c52a30ad8b6d51513af40834fccca1ff9152376
obnoxious <obnoxious@is.too.gay>
2021-11-28 15:45:16 -0800

    gggggg

--- src/strings.rs
+++ src/strings.rs
@@ -1,1 +1,16 @@
-pub fn run() {}
+// Primitive str = immutable fixed length string somewhere in memory
+// String = growable, heap-allocated data structure - use when you need to modify or own string data
+
+pub fn run() {
+    let mut hello = String::from("Hello ");
+
+    // Get length
+    println!("Length: {}", hello.len());
+
+    // Push char
+    hello.push('W');
+
+    // Push string
+    hello.push_str("orld!");
+
+    println!("{}", hello);
